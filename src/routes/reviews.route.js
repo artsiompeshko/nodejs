df49c5be4cd2f9e7cfabc4cc378115236d01d03d
Artsiom Peshko <Artsiom_Peshko@epam.com>
2017-11-02 12:50:19 +0300

    add express task

--- src/routes/reviews.route.js
+++ src/routes/reviews.route.js
@@ -2,13 +2,13 @@ import { Router } from 'express';
 
 const router = Router();
 
-// router.use('/', (req, res, next) => {
-//   if (!req.product) {
-//     res.sendStatus(404);
-//   }
-//
-//   next();
-// });
+router.use('/', (req, res, next) => {
+  if (!req.product) {
+    res.sendStatus(404);
+  }
+
+  next();
+});
 
 router.get('/', (req, res) => {
   res.json(req.product.reviews);
